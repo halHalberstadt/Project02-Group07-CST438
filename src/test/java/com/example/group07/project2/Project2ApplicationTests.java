a5561ffaf0bdd6c06ffa36fc5788b15db6dab839
Daisy Rocha-Montanez <72002539+daisyrocha@users.noreply.github.com>
2022-03-02 18:45:16 -0800

    Add files via upload
    Thyme Leaf has not been added as dependency, since I'm not sure if it's what we're going to be using

---
+++ src/test/java/com/example/group07/project2/Project2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group07.project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
