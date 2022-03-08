d27447be48d25f6857358bee062e46cd3de4a8d1
Daisy Rocha-Montanez <72002539+daisyrocha@users.noreply.github.com>
2022-03-07 20:52:54 -0800

    List & ListRepository
    Missing ListApi

---
+++ src/main/java/com/example/group07/project2/ListRepository.java
@@ -0,0 +1,13 @@
+package com.example.group07.project2;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * This is a repository that is a repository for our List object that we
+ * just created.
+ * Used for accessing info from our database. Pairs up with the List that we just created
+ * and the ID that we use to uniquely identify that this is a repository, (Integer)
+ * We will have an empty interface and it will work
+ */
+public interface ListRepository extends CrudRepository<List, Integer> {
+}
