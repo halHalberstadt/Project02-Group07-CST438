60aeb4a6a7183e9efb67444735fc0160abd8e685
halHalberstadt <halberstadtsterling@gmail.com>
2022-03-08 11:40:36 -0800

    I fixed the error of no "userList" entities so this can build, will make a branch off this. Don't want to add non-functioning files to the main branch.

---
+++ src/main/java/com/example/group07/project2/UserList.java
@@ -0,0 +1,49 @@
+package com.example.group07.project2;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserList {
+
+    @Id // this is our primary key
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+
+    String title;
+    String description;
+
+    /**
+     * Getters and Setters for our private variables
+     * This is the data we will be inserting into our entity
+     */
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer id) {
+        this.userId = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setList(String list) {
+        this.title=list;
+    }
+}
