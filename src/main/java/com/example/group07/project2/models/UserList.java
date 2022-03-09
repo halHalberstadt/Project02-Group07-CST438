46d5ef2511bb0719739c8ea4fa920b47e232a1d7
halHalberstadt <halberstadtsterling@gmail.com>
2022-03-08 18:39:52 -0800

    edited userList

--- src/main/java/com/example/group07/project2/models/UserList.java
+++ src/main/java/com/example/group07/project2/models/UserList.java
@@ -1,54 +1,50 @@
-package com.example.group07.project2.models;
+package com.example.group07.project2;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 /**
  * We are creating a UserList entity that represents
- * a user list in our database. This is used to sort
- * the lists that the user wants.
+ * the list created by the user
  */
 @Entity
 public class UserList {
-
     @Id // this is our primary key
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer userId;
+    private Integer userListId;
 
-    String title;
-    String description;
+    private Integer userId;
+    private List<UserList> list;
+
 
     /**
      * Getters and Setters for our private variables
      * This is the data we will be inserting into our entity
      */
+    public Integer getUserListId() {
+        return userListId;
+    }
+
+    public void setUserListId(Integer userListId) {
+        this.userListId = userListId;
+    }
+
     public Integer getUserId() {
         return userId;
     }
 
-    public void setUserId(Integer id) {
-        this.userId = id;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 
-    public String getTitle() {
-        return title;
+    public List<UserList> getList() {
+        return this.list;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setList(String list) {
-        this.title=list;
+    public void setList(List<UserList> list) {
+        this.list = list;
     }
 }
