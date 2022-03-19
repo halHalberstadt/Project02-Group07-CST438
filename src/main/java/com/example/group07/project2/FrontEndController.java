a63504b9593f8a4e559960d00ab405bfc4dee02c
halHalberstadt <halberstadtsterling@gmail.com>
2022-03-18 17:18:59 -0700

    removed commenting

--- src/main/java/com/example/group07/project2/FrontEndController.java
+++ src/main/java/com/example/group07/project2/FrontEndController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontEndController {
-//
-//    @RequestMapping("/")
-//    String Home(Model model) {
-//        return "home";
-//    }
+
+    @RequestMapping("/")
+    String Home(Model model) {
+        return "home";
+    }
 
 }
