4b893269ca47e8443a22f09d5c090c44e50cbc80
Daisy Rocha-Montanez <72002539+daisyrocha@users.noreply.github.com>
2022-03-22 20:41:30 -0700

    Revert "updated /addUser parameter to save "name""

--- src/main/java/com/example/group07/project2/UserApi.java
+++ src/main/java/com/example/group07/project2/UserApi.java
@@ -3,6 +3,7 @@ package com.example.group07.project2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -27,7 +28,6 @@ public class UserApi {
      * This path will actually return all users without
      * us having to type so much code!
      */
-
     @GetMapping(path = "/allUsers")
     public @ResponseBody Iterable<String> getAllUsers() {
         ArrayList<String> userList = new ArrayList<>();
@@ -39,6 +39,7 @@ public class UserApi {
         return userList;
     }
 
+
     /**
      * This PostMapping will allow us to add users to our
      * database entity
@@ -46,15 +47,52 @@ public class UserApi {
      * Literally this is all we need to add stuff to our database
      */
     @PostMapping(path="/addUser")
-    public @ResponseBody String addUser (@RequestParam String name, @RequestParam String username, @RequestParam String password) {
+    public @ResponseBody String addUser (@RequestParam String username, @RequestParam String password, Model model) {
         User user = new User();
-        user.setName(name);
+        for(User u : userRepository.findAll()) {
+            if (u.getUsername().toString().equals(username))
+                return "username taken!";
+        }
+
+        if(username.isBlank() || password.isBlank())
+            return "Invalid username or password";
+
         user.setUsername(username);
         user.setPassword(password);
 
         userRepository.save(user);
 
-        return "Saved User!";
+
+        return "signUp";
+
+        //return "signUp";
+    }
+
+    /**
+     * This path will return just the user from the id in the url,
+     * or returns a not found message,
+     * NOTE: password is not shown when grabbed.
+     */
+    @GetMapping(path = "/getUserById")
+    public @ResponseBody String getUserById(@RequestParam @NonNull Integer id) {
+        for(User i:userRepository.findAll()){
+            if(i.getUserId().equals(id))
+                return "UserId: " + i.getUserId() + " , " +
+                        "name: " + i.getName() + " , " +
+                        "username: \"" + i.getUsername() + "\"";
+        }
+        return "User not found.";
+    }
+
+    @GetMapping(path = "/getUserByUsername")
+    public @ResponseBody String getUserByUsername(@RequestParam @NonNull String username) {
+        for(User i:userRepository.findAll()){
+            if(i.getUsername().equals(username))
+                return "UserId: " + i.getUserId() + ", " +
+                        "name: " + i.getName() + ", " +
+                        "username: \"" + i.getUsername() + "\"";
+        }
+        return "User not found.";
     }
 
     @GetMapping(path="/findByName")
@@ -88,12 +126,12 @@ public class UserApi {
      * this updates the fields of a User on the system.
      * TODO: add admin restriction/ from that same user
      */
-    @GetMapping("/UpdateUser")
-    public @ResponseBody String UpdateUser(@RequestParam Integer userID,
-                                           @RequestParam(required = false) String username,
-                                           @RequestParam(required = false) String password) {
+    @GetMapping("/updateUser")
+    public @ResponseBody String updateUser(@RequestParam @NonNull Integer userID,
+                                           @RequestParam String username,
+                                           @RequestParam String password) {
         for(User currUser: userRepository.findAll()){
-            if(currUser.getUserId().equals(userID)){
+            if(currUser.getUserId().toString().equals(userID.toString())){
                 if(!username.isBlank())
                     currUser.setUsername(username);
 
